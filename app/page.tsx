38d0a5399c76b3360d7fd7e6b1578b0bccb2e402
youssifkhalid <145773104+youssifkhalid@users.noreply.github.com>
2025-01-26 19:47:37 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,15 @@
 
+import React from "react"
+//import VideoUploader from "../app_v0"; //This line is incorrect and needs to be updated.
+import VideoUploader from "../app_v0modified"
+
+function App() {
+  return (
+    <div className="App">
+      <VideoUploader />
+    </div>
+  )
+}
+
+export default App
+
